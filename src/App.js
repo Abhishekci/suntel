4919077bcaa74e499ecf955eba69cc6352e47292
Abhishek Itagimath <abhishekitagimath24@gmail.com>
2023-02-23 20:27:07 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import React, { useState } from "react";
-import './App.css';
-import { Login } from "./login";
-import { Register } from "./register";
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./layout/Navbar";
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AddUser from "./Users/AddUser";
+import EditUser from "./Users/EditUser";
+import ViewUser from "./Users/ViewUser";
 
 function App() {
-  const [currentForm, setCurrentForm] = useState('login');
-
-  const toggleForm = (formName) => {
-    setCurrentForm(formName);
-  }
-
   return (
     <div className="App">
-      {
-        currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
-      }
+      <Router>
+        <Navbar />
+
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/adduser" element={<AddUser />} />
+          <Route exact path="/edituser/:id" element={<EditUser />} />
+          <Route exact path="/viewuser/:id" element={<ViewUser />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
